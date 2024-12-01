e01e98fe64777a1c92edb2a87a53b2a87b636b73
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-12-01 21:04:55 +0100

    add htmx setup

--- scripts.py
+++ scripts.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+from pathlib import Path
 
 def manage(*args):
     subprocess.run(["python", "manage.py"] + list(args))
@@ -10,3 +12,36 @@ def dev():
 def migrate():
     manage("migrate")
     manage("makemigrations")
+
+def setup_HTMX():
+    print("Setting up HTMX")
+
+    htmx = Path("static/htmx.min.js")
+    if htmx.exists():
+        print("htmx already installed")
+        return
+    
+    static_folder = Path("static")
+    if not static_folder.exists():
+        os.mkdir(static_folder)
+
+    
+    download_file(
+        "https://unpkg.com/htmx.org@2.0.3",
+        "htmx.min.js",
+    )
+
+def download_file(url: str, name: str) -> None:
+    print(f"Downloading..\n{name}\n from:\n{url}")
+    proc = subprocess.run(
+        [
+            "curl",
+            "--fail",
+            "--location",
+            url,
+            "-o",
+            f"static/{name}"
+        ]
+    )
+    if proc.returncode != 0:
+        raise SystemExit(1)
