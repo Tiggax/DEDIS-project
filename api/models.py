6ddd3d3d40490ff4f9b33fe8d87b8583a4ee823b
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-12-29 22:54:01 +0100

    add permisisons and ranks to user

--- api/models.py
+++ api/models.py
@@ -3,7 +3,24 @@ from django.db import models
 from django_summernote.fields import SummernoteTextField
 
 class ClimbUser(AbstractUser):
-    pass
+    PERMISSIONS = (
+        ("U","Unapproved"),
+        ("A", "Approved"),
+        ("M", "Moderator")
+    )
+
+    RANKS = (
+        ("A", "Apprentice"),
+        ("J", "Junior"),
+        ("S", "Senior"),
+        ("P", "Alpinist"),
+        ("I", "Alpinist Instructor")
+    )
+
+    permission = models.CharField(max_length=1, choices=PERMISSIONS, default="U")
+    rank = models.CharField(max_length=1, choices=RANKS, default="A")
+    profile_pic = models.FileField(null=True, blank=True)
+
     def __str__(self):
         return self.username
 
