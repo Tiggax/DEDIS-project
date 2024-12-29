7d83bbe3e619439137e3edba4d09ecbf00aed3be
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2024-12-29 02:37:23 +0100

    stage initial model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(Mountain)
+admin.site.register(Route)
+admin.site.register(Report)
+admin.site.register(GalleryImage)
+admin.site.register(Comment)
+admin.site.register(PostTag)
+admin.site.register(NewsPost)
