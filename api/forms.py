52eb877b76361e5e20549b8f0320106426a54d69
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-01-07 20:45:39 +0100

    move and fix forms for user registration

---
+++ api/forms.py
@@ -0,0 +1,17 @@
+from api import settings
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+from api.models import ClimbUser
+
+class ClimbUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['username'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+    class Meta(UserCreationForm.Meta):
+        model = ClimbUser
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
+            
