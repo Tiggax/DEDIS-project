558b114f04aec968a466ab1000f9e5b9492a8226
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-01-07 21:33:47 +0100

    modify the user page to have proper logout

--- api/settings/default.py
+++ api/settings/default.py
@@ -137,6 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+LOGIN_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # BOOTSTRAP5
