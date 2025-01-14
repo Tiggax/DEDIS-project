fc4050a6d870e4c642d0d23bf594c989ffa30579
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-01-14 16:00:53 +0100

    change report routing for pages; fix urls for homepage

--- pages/urls.py
+++ pages/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, include
 from . import views
 
 app_name = "pages"
+
+reports =([
+    path("", views.reports, name="all"),
+    path("<uuid:id>", views.report, name="get"),
+], "reports")
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("about_us", views.about_us, name="about_us"),
     path("news", views.news, name="news"),
     path("news/<uuid:id>", views.news_page, name = "news_page"),
-    path("reports", views.reports, name="reports"),
-    path("reports/<uuid:id>", views.report, name="report"),
+    path("reports/", include(reports)),
 ]
