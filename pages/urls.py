07789350bbe433b144c220e2d26251cd2048577e
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-01-14 17:57:31 +0100

    change news routing

--- pages/urls.py
+++ pages/urls.py
@@ -3,16 +3,20 @@ from . import views
 
 app_name = "pages"
 
-reports =([
+reports = ([
     path("", views.reports, name="all"),
     path("<uuid:id>", views.report, name="get"),
     path("create", views.report_form, name="create")
 ], "reports")
 
+news = ([
+    path("", views.news, name="all"),
+    path("<uuid:id>", views.news_page, name = "get"),
+], "news")
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("about_us", views.about_us, name="about_us"),
-    path("news", views.news, name="news"),
-    path("news/<uuid:id>", views.news_page, name = "news_page"),
+    path("news", include(news)),
     path("reports/", include(reports)),
 ]
