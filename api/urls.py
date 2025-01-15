728a93d508ef18ef7736e582cdcb8d2a9053d0b3
Tilen Gimpelj <66419530+Tiggax@users.noreply.github.com>
2025-01-15 09:59:21 +0100

    make media only in debug

--- api/urls.py
+++ api/urls.py
@@ -67,7 +67,8 @@ if settings.DEBUG:
         settings.STATIC_URL,
         document_root = settings.STATIC_ROOT
     )
-urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root = settings.MEDIA_ROOT
-    )
+
+    urlpatterns += static(
+            settings.MEDIA_URL,
+            document_root = settings.MEDIA_ROOT
+        )
